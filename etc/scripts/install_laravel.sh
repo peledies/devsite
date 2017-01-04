7395169d2a7e143f6204942c7f4d7bc974060035
Deac <deac.karns@gmail.com>
2017-01-03 20:01:24 -0500

    Add laravel project to setup

---
+++ etc/scripts/install_laravel.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+pushd $(dirname $0) > /dev/null; SCRIPTPATH=$(pwd); popd > /dev/null
+INITDIR=`pwd`
+
+project=$1
+source $SCRIPTPATH/assets/info_box.sh
+source $SCRIPTPATH/assets/pretty_tasks.sh
+
+if [ -z "$1" ];then
+  echo "${red}You must specify a project name to create.${default}"
+  exit 1
+fi
+
+# Clone the current opencart repo
+echo_start
+echo -n "${gold}Cloning the current opencart repo${default}"
+  composer create-project --prefer-dist laravel/laravel tmp/ > /dev/null 2>&1
+test_for_success $?
+
+# Move files from tmp directory
+echo_start
+echo -n "${gold}Moving files from tmp directory${default}"
+  mv tmp/* .
+test_for_success $?
+
+# Create new nginx server configuration
+echo_start
+echo -n "${gold}Creating Vagrant configuration file${default}"
+
+cat <<EOF > $INITDIR/Vagrantfile
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "pinwheel-development/centos-minimal-6_5"
+  config.vm.box_url = "http://vagrant.sdicgdev.com/pinwheel-development-centos-6-5.box"
+  
+  config.vm.network :forwarded_port, host: 4567, guest: 80
+  config.vm.network "private_network", ip: "192.168.2.2"
+  config.vm.provision :shell, :path => "bootstrap.sh"
+end
+EOF
+test_for_success $?
+
+# Remove tmp directory
+echo_start
+echo -n "${gold}Removing tmp directory${default}"
+  rm -rf tmp
+test_for_success $?
+
+# Start Vagrant
+vagrant up
--- etc/scripts/setup.sh
+++ etc/scripts/setup.sh
@@ -16,6 +16,11 @@ init_opencart_site() {
   build_docker_containers
 }
 
+init_laravel_site() {
+  source $SCRIPTPATH/install_laravel.sh $project
+  build_docker_containers
+}
+
 build_docker_containers(){
   docker-compose build
 }
@@ -29,15 +34,17 @@ echo "${default}"
 _menu () {
   echo "${green}  Choose an Option"
   echo " ===================${normal}"
-  echo "${magenta} 1 ${default}- This is a ${red}Slim App${default}"
-  echo "${magenta} 2 ${default}- This is an ${red}Opencart site${default}"
+  echo "${magenta} 1 ${default}- This is a ${red}Slim App (Docker)${default}"
+  echo "${magenta} 2 ${default}- This is a ${red}Laravel App (Vagrant)${default}"
+  echo "${magenta} 3 ${default}- This is an ${red}Opencart site (Docker)${default}"
   echo "${magenta} x ${default}- Exit"
 
   while true; do
     read -p "${cyan} Select an option from the list above: ${gold}" answer
     case $answer in
       1 ) clear; init_slim_app; break;;
-      2 ) clear; init_opencart_site; break;;
+      2 ) clear; init_laravel_app; break;;
+      3 ) clear; init_opencart_site; break;;
       x ) clear; exit; break;;
 
       * ) echo "Please select a valid option.";;
