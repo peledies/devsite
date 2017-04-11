03d7804eae81049c98e5a78aa532332445a33261
Deac <deac.karns@gmail.com>
2017-04-10 21:38:25 -0400

    escape dollar sign

--- etc/scripts/install_laravel.sh
+++ etc/scripts/install_laravel.sh
@@ -35,10 +35,10 @@ fi
   if [ "$image" == "sfp" ]
     then
     use_image='config.vm.box = "kacomp_v4"
-config.vm.box_url = "http://dash.sfp.cc/kacomp_v4.box"'
+  config.vm.box_url = "http://dash.sfp.cc/kacomp_v4.box"'
   else 
     use_image='config.vm.box = "hashicorp/precise64"
-config.vm.box_url = "http://files.vagrantup.com/precise64.box"'
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"'
   fi
 
 # Clone the current laravel repo
@@ -78,7 +78,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.synced_folder "./vendor", "/home/fujita/vendor"
   config.vm.synced_folder "./etc", "/home/fujita/"
 
-$bashlaunch = <<SCRIPT
+\$bashlaunch = <<SCRIPT
     cp /etc/httpd/conf/httpd.conf /tmp/httptmp
     sed -e "s/#EnableSendfile off/EnableSendfile off/" /tmp/httptmp
     cat /tmp/httptmp > /etc/httpd/conf/httpd.conf
